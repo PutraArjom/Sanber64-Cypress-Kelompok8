5cb1400d5b1a2fcb8e640162fcf6cc7098865bc0
Hendianto Eka Saputra <94177897+PutraArjom@users.noreply.github.com>
2025-02-24 17:20:59 +0700

    Update Login part2

--- cypress/e2e/magento/login.cy.js
+++ cypress/e2e/magento/login.cy.js
@@ -10,13 +10,27 @@ describe("template spec", () => {
     cy.get('.base').should('have.text','My Account')
   });
 
+  it("Fail Login - Forgot @gmail", () => {
+    cy.login('Sukri','SukriPeod1')
+    cy.get('#email-error').should('be.visible')
+  });
+
+  it("Fail Login - Wrong Email True Pass", () => {
+    cy.login('Ahi@gmail.com','SukriPeod1')
+    cy.get('.message-error > div').should('have.text',"The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.")
+  });
+    it("Fail Login - True Email Fail Pass", () => {
+    cy.login('Skuri@gmail.com','Peod1')
+    cy.get('.message-error > div').should('have.text',"The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.")
+  });
+
   it("Failed Login - The user did not enter email and password", () => {
     loginPage.buttonSigIn()
     cy.get('#email-error').should('contain.text','This is a required field')
     cy.get('#pass-error').should('contain.text','This is a required field')
   });
 
-  it("Failed Login - The user did not enter email", () => {
+  it.only("Failed Login - The user did not enter email", () => {
     loginPage.inputPassword()
     loginPage.buttonSigIn()
     cy.get('#email-error').should('contain.text','This is a required field')
@@ -30,16 +44,16 @@ describe("template spec", () => {
 
   it("Try Forgot password", () => {
     loginPage.forgotPass()
-    loginPage.inputUser()
+    cy.get('#email_address').type("Sukri@gmail.com")
     loginPage.buttonResetPassword()
     cy.url().should('include','/account/login') 
-    cy.get('.message-success > div').should('be.visible')
+    cy.get('.message-success > div').should('have.text','If there is an account associated with Sukri@gmail.com you will receive an email with a link to reset your password.')
   });
 
   it("Try Forgot password - No input Email", () => {
     loginPage.forgotPass()
     loginPage.buttonResetPassword()
-    cy.get('.message-error > div').should('be.visible')
-    //cy.get('#email_address-error').should('be.visible')
+    //cy.get('.message-error > div').should('be.visible')
+    cy.get('#email_address-error').should('be.visible')
   });
 });
