caa0358ac2ec0b8b0c1d606dd59cdd8975a7ecc7
adzkiaayuni96 <adzkiaayuni96@gmail.com>
2025-03-12 06:22:42 +0700

    choose product and update shopping cart

--- cypress/e2e/magento/chooseProductUpdateCart.cy.js
+++ cypress/e2e/magento/chooseProductUpdateCart.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Choose Product and Update Shopping Cart', () => {
+  it('Choose Product Successfully', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/login/')
+    cy.get('Sukri@gmail.com','SukriPeod1')
+    cy.get('Sukri','SukriPeod1')
+    cy.get('#send2').click()
   })
+
 })
